405ffeea21b0ff2a77e395ce65378614af8d329a
CydiaAbdullah <40449601+CydiaAbdullah@users.noreply.github.com>
2019-03-29 13:50:59 +0300

    Create unRP.sh

---
+++ debs/ReProvision/unRP.sh
@@ -0,0 +1,15 @@
+rm /jb/entitlements.xml
+rm /jb/Library/LaunchDaemons/com.matchstic.reprovisiond.plist
+rm /Applications/ReProvision.app
+rm -rf /jb/usr/bin/reprovisiond /Applications/ReProvision.app
+echo "please waiting..."
+echo "اصبر شوي بس...🌝"
+uicache
+echo "Application deleted successful🥳"
+echo " Thank you for trusting us❤️، 
+Twitter @CydiaAbdullah"
+echo " .
+.
+.
+.
+"
